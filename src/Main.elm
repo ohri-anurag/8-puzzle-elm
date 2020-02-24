547436b00949b109f52e508fffdc58108527069a
Anurag Ohri <anuragohri92@gmail.com>
2020-02-24 22:48:44 +0530

    Added a basic model for 8-puzzle

--- src/Main.elm
+++ src/Main.elm
@@ -5,21 +5,29 @@ import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+  Browser.sandbox { init = initModel, update = update, view = view }
 
 type Msg = Increment | Decrement
 
-update msg model =
-  case msg of
-    Increment ->
-      model + 1
+initModel = [1,2,3,4,5,6,7,8,0]
 
-    Decrement ->
-      model - 1
+update msg model = model
 
-view model =
+view model = div []
+  [
   div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
+    [ div [] [ text "1" ]
+    , div [] [ text "2" ]
+    , div [] [ text "3" ]
+    ],
+  div []
+    [ div [] [ text "1" ]
+    , div [] [ text "2" ]
+    , div [] [ text "3" ]
+    ],
+  div []
+    [ div [] [ text "1" ]
+    , div [] [ text "2" ]
+    , div [] [ text "3" ]
     ]
+  ]
