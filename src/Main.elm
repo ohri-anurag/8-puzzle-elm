7724cebc05c00b29a931ef86453ee6bada447d6b
Anurag Ohri <anuragohri92@gmail.com>
2020-02-25 20:57:29 +0530

    Added movement in accordance with the clicked div

--- src/Main.elm
+++ src/Main.elm
@@ -1,21 +1,26 @@
 module Main exposing(..)
 
+import Basics exposing ((|>))
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Attributes exposing (attribute)
 import Html.Events exposing (onClick)
-import List exposing ((::), drop, isEmpty, map, take)
+import List exposing ((::), drop, indexedMap, isEmpty, map, sum, take)
 import String exposing (fromInt)
+import Tuple exposing (pair)
 
 main =
   Browser.sandbox { init = initModel, update = update, view = view }
 
-type Msg = Increment | Decrement
+
+type Operation = Up | Down | Left | Right | NoOp
+
 
 type alias Model =
   { model: List Int
   , zero: Int
   }
+
 
 initModel : Model
 initModel =
@@ -23,17 +28,99 @@ initModel =
   , zero = 8
   }
 
-update msg model = model
 
-view model = div [] (viewify model.model)
+update : Operation -> Model -> Model
+update msg model =
+  case msg of
+    Up ->
+      updateModel (-3) model
 
-viewify list =
+    Down ->
+      updateModel 3 model
+
+    Left ->
+      updateModel (-1) model
+
+    Right ->
+      updateModel 1 model
+
+    NoOp ->
+      model
+
+
+updateModel : Int -> Model -> Model
+updateModel movement model =
+  let
+    newZero = model.zero + movement
+  in
+  { model
+  | model = swap model.zero newZero model.model
+  , zero = newZero
+  }
+
+
+swap : Int -> Int -> List Int -> List Int
+swap i j list =
+  let
+    swapSum =
+      list
+        |> indexedMap
+          (\k n ->
+            if k == i || k == j
+              then n
+              else 0
+          )
+        |> sum
+  in
+  list
+    |> indexedMap
+      (\k n ->
+        if k == i || k == j
+          then swapSum - n
+          else n
+      )
+
+view : Model -> Html Operation
+view model =
+  model.model
+    |> indexedMap pair
+    |> rowify model.zero
+    |> div []
+
+
+rowify : Int -> List (Int, Int) -> List (Html Operation)
+rowify zeroPos list =
   if isEmpty list
     then []
-    else div [] (map divify (take 3 list)) :: viewify (drop 3 list)
+    else div [] (map (divify zeroPos) (take 3 list)) :: rowify zeroPos (drop 3 list)
 
-divify num = div [
-    if num == 0
-      then attribute "class" "zero"
-      else attribute "class" "cell"
+
+divify : Int -> (Int, Int) -> Html Operation
+divify zeroPos (pos, num) =
+  div [
+    attribute "class" <|
+      if num == 0
+        then "zero"
+        else "cell",
+    onClick <|
+      if isNeighbour pos zeroPos
+        then whichNeighbour pos zeroPos
+        else NoOp
   ] [ text (fromInt num) ]
+
+
+isNeighbour : Int -> Int -> Bool
+isNeighbour pos1 pos2 =
+  abs(pos1 - pos2) == 1 || abs(pos1 - pos2) == 3
+
+
+whichNeighbour : Int -> Int -> Operation
+whichNeighbour pos zeroPos =
+  if pos - zeroPos == -3
+    then Up
+    else if pos - zeroPos == -1
+      then Left
+      else if pos - zeroPos == 1
+        then Right
+        else Down
+
